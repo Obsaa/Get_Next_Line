262a27723568e05e0c7b10c563328846f86ad1e3
Obsaa Abdalhalim <obsaaabdalhalim@Obsaas-MacBook-Pro.local>
2017-05-12 14:35:25 -0700

    incremented pointer instead of adding to the pointer space like in my strchr

--- get_next_line.c
+++ get_next_line.c
@@ -12,43 +12,51 @@
 
 #include "get_next_line.h"
 
-static int			ft_check_stock(char **stock, char **line)
+static char		*loc_pointer_chr(const char *s, int c)
 {
-	char			*var;
+	while (*s != (char)c && *s != '\0')
+		s++;
+	if (*s == (char)c)
+		return ((char *)s);
+	return (NULL);
+}
 
-	var = NULL;
-	if (*stock)
+static int			ft_check_bucket(char **bucket, char **line)
+{
+	char			*a;
+
+	a = NULL;
+	if (*bucket)
 	{
-		if ((var = ft_strchr((const char*)*stock, '\n')))
+		if ((a = loc_pointer_chr((const char*)*bucket, '\n')))
 		{
-			*line = ft_strsub((char const*)*stock, 0, var - *stock);
-			ft_memmove(*stock, var + 1, ft_strlen(var));
-			return (1);
-		}
+			*line = ft_strsub((char const*)*bucket, 0, a - *bucket);
+			ft_memmove(*bucket, a + 1, ft_strlen(a));
+ 		}
 	}
 	return (0);
 }
 
-static	int			cpy_fd_in_stock(int fd, char **stock, char **line)
+static	int			cpy_fd_in_bucket(int fd, char **bucket, char **line)
 {
 	char			buf[BUFF_SIZE + 1];
 	int				ret;
-	char			*var;
+	char			*a;
 
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		if (ret == -1)
 			return (-1);
 		buf[ret] = '\0';
-		var = *stock;
-		if (*stock)
+		a = *bucket;
+		if (*bucket)
 		{
-			*stock = ft_strjoin((char const*)var, (char const*)buf);
-			free(var);
+			*bucket = ft_strjoin((char const*)a, (char const*)buf);
+			free(a);
 		}
 		else
-			*stock = ft_strdup((const char*)buf);
-		if (ft_check_stock(stock, line))
+			*bucket = ft_strdup((const char*)buf);
+		if (ft_check_bucket(bucket, line))
 			return (1);
 	}
 	return (0);
@@ -61,9 +69,9 @@ int					get_next_line(int const fd, char **line)
 
 	if (!line || fd < 0)
 		return (-1);
-	if (ret[fd] && ft_check_stock(&ret[fd], line))
+	if (ret[fd] && ft_check_bucket(&ret[fd], line))
 		return (1);
-	result = cpy_fd_in_stock(fd, &ret[fd], line);
+	result = cpy_fd_in_bucket(fd, &ret[fd], line);
 	if (result != 0)
 		return (result);
 	if (ret[fd] == NULL || ret[fd][0] == '\0')
